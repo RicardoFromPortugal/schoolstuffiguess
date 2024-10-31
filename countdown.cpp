cd8fa2ceb7a385fe74157ddb37344c5aecc27ffa
Ricardo <70768594+RicardoFromPortugal@users.noreply.github.com>
2024-10-31 17:46:25 +0000

    Add files via upload

---
+++ countdown.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int a;
+using namespace std;
+int main(){
+	cout << "\nnumber\n";
+	cin >> a;
+	cout << "\n";
+	for (a; a != -1; a--){
+	cout << a << "\n";
+	sleep(1);}
+	main();}
