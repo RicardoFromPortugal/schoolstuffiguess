04e80899db1ec29972c228fd1c8261f21f8ba1fd
Ricardo <70768594+RicardoFromPortugal@users.noreply.github.com>
2024-10-31 18:02:32 +0000

    Add files via upload

---
+++ fat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int n,m;
+int main(){
+	cout << "\nquantas pessoas e numero fatias\n";
+	cin >> n;
+	cin >> m;
+	cout << "\npode dar " << (m/n) - (m%n) << " fatias para cada familiar\nresto para dar para refood:" << m%n;
+	sleep(60); 
+}
