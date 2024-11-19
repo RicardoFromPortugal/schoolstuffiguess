b4089cb46a79420678536b0e47e07ad96c97f20a
Ricardo <70768594+RicardoFromPortugal@users.noreply.github.com>
2024-11-19 16:04:12 +0000

    Add files via upload

---
+++ alturamodi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int a[30],b[30],i,c,av;
+int main(){
+	for (i = 0; i < 30; i++){
+		cout << "\nidade aluno " << i+1 << ":";
+		cin >> a[i];
+		cout << "\naltura: ";
+		cin >> b[i];
+		if(a[i]>=13){
+		c++;}}
+		c=0;
+	for (i=0;i<c;i++){
+		if (a[i] >= 13 && b[i]<av){
+			c++;}}
+	av=av/c;
+	cout << "\naverage is: " << av;
+	cout << "\nstudents under the average height is: " << c;
+		sleep(60);}
