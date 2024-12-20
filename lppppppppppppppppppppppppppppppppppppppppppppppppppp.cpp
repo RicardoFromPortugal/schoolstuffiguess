9005a714b08e62efa93739fdaaf56d81d424a5ba
Ricardo <70768594+RicardoFromPortugal@users.noreply.github.com>
2024-12-20 14:54:31 +0000

    Add files via upload

---
+++ lppppppppppppppppppppppppppppppppppppppppppppppppppp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+char frase[40000];
+int a,i;
+int main(){
+	cout << "how many letters for word/frase: ";
+	cin >> noskipws >> a;
+	cout << "alright, type it in: ";
+	for(i=0;i<a+1;i++){
+		cin >> frase[i];}
+	cout << "translated: ";
+	for(i=0;i<a+1;i++){
+		cout << frase[i];
+		if(frase[i]=='a'||frase[i]=='e'||frase[i]=='i'||frase[i]=='o'||frase[i]=='u'||frase[i]=='A'||frase[i]=='E'||frase[i]=='I'||frase[i]=='O'||frase[i]=='U'){
+			cout << "p" << frase[i];}}
+	sleep(60);}
